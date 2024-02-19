622335bb41b9bca6c85d1b8cc7d7e958af75b1f0
Julia Nowak <93201467+julinek999@users.noreply.github.com>
2024-02-19 13:48:36 +0000

    Added a background

--- Assets/Player/Sprites/PlayerShipController.cs
+++ Assets/Player/Sprites/PlayerShipController.cs
@@ -7,20 +7,33 @@ public class PlayerShipController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
 
-    private void Update()
+private void Update()
+{
+    float moveInput = Input.GetAxis("Vertical");
+    Vector2 movement = transform.up * moveInput * speed * Time.deltaTime;
+    transform.Translate(movement);
+
+    float rotateInput = Input.GetAxis("Horizontal");
+    transform.Rotate(Vector3.back * rotateInput * rotationSpeed * Time.deltaTime);
+
+    // Check if the player is out of screen bounds
+    CheckBounds();
+
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        float moveInput = Input.GetAxis("Vertical");
-        Vector2 movement = transform.up * moveInput * speed * Time.deltaTime;
-        transform.Translate(movement);
-
-        float rotateInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.back * rotateInput * rotationSpeed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Shoot();
-        }
+        Shoot();
     }
+}
+
+void CheckBounds()
+{
+    Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+    if (viewportPosition.y < 0) 
+    {
+        Vector3 newPosition = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1.1f, 0));
+        transform.position = newPosition;
+    }
+}
 
     void Shoot()
     {
