1d705d3acc91fd8436dc958cd24b5566e659062f
Julia Nowak <93201467+julinek999@users.noreply.github.com>
2024-02-28 13:12:22 +0000

    Bullets are now destroying asteroids

--- Assets/Asteroid/Sprites/AsteroidBehavior.cs
+++ Assets/Asteroid/Sprites/AsteroidBehavior.cs
@@ -7,16 +7,14 @@ public class AsteroidBehaviour : MonoBehaviour
 
     void Start()
     {
-
         float speed = Random.Range(minSpeed, maxSpeed);
         Vector2 direction = Random.insideUnitCircle.normalized;
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = direction * speed;
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Bullet") || other.CompareTag("Player"))
+        if (other.CompareTag("Bullet"))
         {
             Destroy(gameObject);
         }
